94475da3946cd0af6f42a02428597952b2826e53
Franckbzh56 <Josselin56@orange.fr>
2017-04-25 17:58:01 +0200

    Ajout code pour token

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.get('/webhook', function(req, res, next) {
+    console.log(req.query);
+    if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'plop') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 module.exports = router;
